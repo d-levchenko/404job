e63b6b04ae14e7f040831c56e9505b86822521db
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-08-26 13:56:01 +0200

    fix: correct user schema initialization and ensure consistent model naming

--- apps/backend/src/models/user.js
+++ apps/backend/src/models/user.js
@@ -1,5 +1,5 @@
 import { model, Schema } from 'mongoose';
-const userSchema = Schema(
+const userSchema = new Schema(
   {
     userType: {
       type: String,
@@ -28,12 +28,16 @@ const userSchema = Schema(
       type: String,
       required: true,
     },
-    githubUrl: { type: String, required: false },
-    linkedin: { type: String, required: false },
-    behanceUrl: { type: String, required: false },
-    logo: { type: String, required: false, default: '' },
-    description: { type: String, required: false, default: '' },
-    websiteUrl: { type: String, required: false, default: '' },
+    githubUrl: { type: String, default: undefined },
+    linkedinUrl: { type: String, default: undefined },
+    behanceUrl: { type: String, default: undefined },
+    savedVacancies: {
+      type: [{ type: Schema.Types.ObjectId, ref: 'Vacancy' }],
+      default: undefined,
+    },
+    logo: { type: String, default: undefined },
+    description: { type: String, default: undefined },
+    websiteUrl: { type: String, default: undefined },
   },
   { collection: 'users', timestamps: true, versionKey: false },
 );
@@ -44,4 +48,4 @@ userSchema.methods.toJSON = function () {
   return obj;
 };
 
-export const User = model('users', userSchema);
+export const User = model('User', userSchema);
