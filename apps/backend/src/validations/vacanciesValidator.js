04a5061be9f8d63e60c43771d8b9e3e9941c29e1
d-levchenko <dl.qubb@gmail.com>
2026-08-26 21:14:43 +0300

    updated validation schema;

--- apps/backend/src/validations/vacanciesValidator.js
+++ apps/backend/src/validations/vacanciesValidator.js
@@ -1,33 +1,21 @@
 import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
-import { Status } from '../constants/status';
 
 const objectIdValidator = (value, helpers) =>
   !isValidObjectId(value) ? helpers.message('Invalid id') : value;
 
 export const getAllVacanciesSchema = {
   [Segments.QUERY]: Joi.object({
-    employerId: Joi.string().custom(objectIdValidator).required(),
-    title: Joi.string().trim().min(2).required(),
-    description: Joi.string().allow('').optional().default(''),
+    page: Joi.number().min(1).required(),
+    perPage: Joi.number().min(1).required(),
 
-    requirements: Joi.string().optional().default(''),
-    duties: Joi.string().optional().default(''),
-    plusWillBe: Joi.string().allow('').optional().default(''),
-    weOffer: Joi.string().allow('').optional().default(''),
+    search: Joi.string().allow('', null),
 
-    industryId: Joi.string().custom(objectIdValidator).required(),
-    experienceLevelId: Joi.string().custom(objectIdValidator).required(),
-    locationId: Joi.string().custom(objectIdValidator).required(),
-    employmentTypeId: Joi.string().custom(objectIdValidator).required(),
+    industry: Joi.string().custom(objectIdValidator).allow('', null),
+    experience: Joi.string().custom(objectIdValidator).allow('', null),
+    location: Joi.string().custom(objectIdValidator).allow('', null),
+    employmentType: Joi.string().custom(objectIdValidator).allow('', null),
 
-    isRemote: Joi.boolean().required().default(false),
-    hotVacancy: Joi.boolean().required().default(false),
-
-    status: Joi.string()
-      .valid(...Status)
-      .required(),
-
-    salaryRange: Joi.string().trim().allow('').optional().default(''),
+    isRemote: Joi.string().valid('true', 'false').allow('', null),
   }),
 };
