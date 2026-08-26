e8396f2bab1f578fdb620758c0e8146a9a1ab78f
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-08-26 17:24:08 +0200

    feat: update authentication endpoints and modify cookie security settings

--- apps/backend/src/services/auth.js
+++ apps/backend/src/services/auth.js
@@ -13,19 +13,19 @@ export const createSession = async userId =>
 export const setSessionCookies = (res, session) => {
   res.cookie('accessToken', session.accessToken, {
     httpOnly: true,
-    secure: true,
+    secure: false,
     sameSite: 'none',
     maxAge: FIFTEEN_MINUTES,
   });
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
-    secure: true,
+    secure: false,
     sameSite: 'none',
     maxAge: ONE_DAY,
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
-    secure: true,
+    secure: false,
     sameSite: 'none',
     maxAge: ONE_DAY,
   });
