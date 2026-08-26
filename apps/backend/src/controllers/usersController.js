766d96e8b44ee87d941daf961c1a3f4b1954a58f
Alena <shining8smaragd@gmail.com>
2026-08-26 17:06:57 +0300

    feat: add users profile endpoints

---
+++ apps/backend/src/controllers/usersController.js
@@ -0,0 +1,41 @@
+import {
+  getCurrentUser,
+  updateCandidateProfile,
+  updateEmployerProfile,
+} from '../services/users.js';
+
+export const getCurrent = async (req, res, next) => {
+  try {
+    const user = await getCurrentUser(req.user._id);
+
+    res.status(200).json({
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateCandidate = async (req, res, next) => {
+  try {
+    const user = await updateCandidateProfile(req.user._id, req.body);
+
+    res.status(200).json({
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateEmployer = async (req, res, next) => {
+  try {
+    const user = await updateEmployerProfile(req.user._id, req.body);
+
+    res.status(200).json({
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
