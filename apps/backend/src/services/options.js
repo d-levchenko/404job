45002a76788e77bc2a44f16d205bf24711a4685c
Віталій <vitaliy19980422@gmail.com>
2026-08-26 18:06:44 +0300

    fix: fix option service

--- apps/backend/src/services/options.js
+++ apps/backend/src/services/options.js
@@ -12,24 +12,6 @@ const modelMap = {
   employmentTypes: EmploymentType,
 };
 
-const fallbackCollectionNames = {
-  locations: ['locations', 'cities'],
-  industries: ['industries', 'industry'],
-  experienceLevels: [
-    'experienceLevels',
-    'experience_levels',
-    'experiencelevels',
-    'experiences',
-  ],
-  employmentTypes: [
-    'employmentTypes',
-    'employment_types',
-    'employmenttypes',
-    'employmentType',
-    'employments',
-  ],
-};
-
 export const getOptionsService = async type => {
   const Model = modelMap[type];
   if (!Model) {
@@ -37,26 +19,5 @@ export const getOptionsService = async type => {
   }
 
   const result = await Model.find();
-  if (result.length > 0) {
-    return result;
-  }
-
-  if (mongoose.connection?.db) {
-    const alternativeNames = fallbackCollectionNames[type] || [];
-    for (const collName of alternativeNames) {
-      try {
-        const docs = await mongoose.connection.db
-          .collection(collName)
-          .find()
-          .toArray();
-        if (docs.length > 0) {
-          return docs;
-        }
-      } catch {
-        // continue trying
-      }
-    }
-  }
-
   return result;
 };
