f1b713f1eefdac9be4d72e29385ca5a28e511f04
vivtorochok <vika17052001@gmail.com>
2026-08-28 22:13:23 +0300

    update-route

--- apps/frontend/components/HomePage/HeroSection/HeroSection.tsx
+++ apps/frontend/components/HomePage/HeroSection/HeroSection.tsx
@@ -1,6 +1,9 @@
+import { getFilterOptions } from '@/lib/optionsApi';
 import css from './HeroSection.module.css';
+import { Location } from '@/types/vacancyType';
 
-function HeroSection() {
+async function HeroSection() {
+  const locations: Location[] = await getFilterOptions('locations');
   return (
     <section className={css.heroSection}>
       <div className={`${css.container} container`}>
@@ -10,6 +13,29 @@ function HeroSection() {
             Ми зібрали найкращі вакансії від провідних IT-компаній України. Твій
             наступний крок до успіху — лише за один клік.
           </p>
+
+          <form className={css.form} action="/vacancies" method="get">
+            <label htmlFor="search">
+              <input className={css.input} name="search" type="text" />
+            </label>
+            <label htmlFor="location">
+              <select
+                className={css.select}
+                name="location"
+                id="location"
+                defaultValue="">
+                <option value="">Уся Україна</option>
+                {locations.map(location => (
+                  <option key={location._id} value={location._id}>
+                    {location.name}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <button className={css.button} type="submit">
+              Знайти вакансію
+            </button>
+          </form>
         </div>
       </div>
     </section>
