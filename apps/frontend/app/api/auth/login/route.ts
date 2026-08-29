a7cefaa7ef038a217cb8782179f5573a2996492b
Ivan Alekseev <alekseeviva1992@gmail.com>
2026-08-29 15:51:47 +0300

    fix route

--- apps/frontend/app/api/auth/login/route.ts
+++ apps/frontend/app/api/auth/login/route.ts
@@ -1,22 +1,43 @@
+import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 
 import { AuthUser } from '@/types/auth';
 import { api } from '../../api';
 
 export const POST = async (request: NextRequest) => {
-  const body = await request.json();
+  try {
+    const body = await request.json();
 
-  const res = await api.post('/auth/login', body);
+    const res = await api.post<AuthUser>('/auth/login', body);
 
-  const response = NextResponse.json<AuthUser>(res.data);
+    const response = NextResponse.json<AuthUser>(res.data, {
+      status: res.status,
+    });
 
-  const setCookie = res.headers['set-cookie'];
+    const setCookie = res.headers['set-cookie'];
 
-  if (setCookie) {
-    for (const cookie of setCookie) {
-      response.headers.append('Set-Cookie', cookie);
+    if (setCookie) {
+      const cookieArr = Array.isArray(setCookie) ? setCookie : [setCookie];
+
+      for (const cookie of cookieArr) {
+        response.headers.append('Set-Cookie', cookie);
+      }
     }
-  }
 
-  return response;
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return NextResponse.json(
+        error.response?.data ?? { message: 'Request failed' },
+        {
+          status: error.response?.status ?? 500,
+        },
+      );
+    }
+
+    return NextResponse.json(
+      { message: 'Internal server error' },
+      { status: 500 },
+    );
+  }
 };
