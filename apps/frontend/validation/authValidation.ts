884d6d6281f96522804efeb7f4a70bf35a127c9c
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-08-29 18:10:50 +0200

    Implement registration form validation and styling updates

---
+++ apps/frontend/validation/authValidation.ts
@@ -0,0 +1,30 @@
+import { UserType } from '@/types/auth';
+import * as Yup from 'yup';
+
+export const RegisterValidation = (type: UserType) =>
+  Yup.object().shape({
+    name:
+      type === 'candidate'
+        ? Yup.string()
+            .trim()
+            .min(2, "Ім'я має бути не менше 2 символів")
+            .max(32, "Ім'я має бути не більше 32 символів")
+            .required("Вкажіть ім'я")
+        : Yup.string(),
+    companyName:
+      type === 'employer'
+        ? Yup.string()
+            .trim()
+            .min(2, 'Назва компанії має бути не менше 2 символів')
+            .max(64, 'Назва компанії має бути не більше 64 символів')
+            .required('Вкажіть назву компанії')
+        : Yup.string(),
+    email: Yup.string()
+      .trim()
+      .email('Електронна адреса має бути валідною, приклад: example@gmail.com')
+      .required('Вкажіть електронну адресу'),
+    password: Yup.string()
+      .min(8, 'Пароль має бути не менше 8 символів')
+      .max(128, 'Пароль має бути не більше 128 символів')
+      .required('Вкажіть пароль'),
+  });
