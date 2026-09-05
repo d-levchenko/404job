2e3a2f3aef20d9161aac4ab48391c5b634d55e1f
vivtorochok <vika17052001@gmail.com>
2026-09-05 15:37:09 +0300

    up

--- apps/frontend/store/authStore.ts
+++ apps/frontend/store/authStore.ts
@@ -5,9 +5,11 @@ interface AuthStoreState {
   user: AuthUser | null;
   userType: UserType;
   isAuthenticated: boolean;
+  isInitialized: boolean;
   setUser: (user: AuthUser) => void;
   setUserType: (userType: UserType) => void;
   setIsAuthenticated: (isAuthenticated: boolean) => void;
+  setIsInitialized: (isInitialized: boolean) => void;
   clear: () => void;
   clearAuthStore: () => void;
 }
@@ -16,10 +18,17 @@ export const useAuthStore = create<AuthStoreState>()(set => ({
   user: null,
   userType: 'candidate',
   isAuthenticated: false,
+  isInitialized: false,
   setUser: (user: AuthUser) => set({ user, userType: user.userType }),
   setUserType: (userType: UserType) => set({ userType }),
   setIsAuthenticated: (isAuthenticated: boolean) => set({ isAuthenticated }),
+  setIsInitialized: (isInitialized: boolean) => set({ isInitialized }),
   clear: () => set({ user: null, isAuthenticated: false }),
   clearAuthStore: () =>
-    set({ user: null, userType: 'candidate', isAuthenticated: false }),
+    set({
+      user: null,
+      userType: 'candidate',
+      isAuthenticated: false,
+      isInitialized: true,
+    }),
 }));
