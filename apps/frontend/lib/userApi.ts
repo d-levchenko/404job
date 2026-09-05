5fff9697ca41391f13f5f001f8ac272fc9188304
d-levchenko <dl.qubb@gmail.com>
2026-09-05 14:18:07 +0300

    type fix for build

--- apps/frontend/lib/userApi.ts
+++ apps/frontend/lib/userApi.ts
@@ -1,12 +1,12 @@
-import { CandidateProfile } from '@/types/userType';
+import { UpdateProfileData } from '@/types/auth';
 import { api } from './api';
 
-export const getCurrentCandidate = async (): Promise<CandidateProfile> => {
-  const { data } = await api.get<{ data: CandidateProfile }>('/users');
+export const getCurrentCandidate = async (): Promise<UpdateProfileData> => {
+  const { data } = await api.get<{ data: UpdateProfileData }>('/users');
   return data.data;
 };
 
-export const updateUser = async (body: CandidateProfile) => {
+export const updateUser = async (body: UpdateProfileData) => {
   const { data } = await api.patch('/users/candidate', body);
   return data;
 };
