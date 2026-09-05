b2480b47d9399ae57d6c1783e08efa973090924d
d-levchenko <dl.qubb@gmail.com>
2026-09-05 18:36:21 +0300

    linter fixes

--- apps/frontend/components/Burger/Burger.tsx
+++ apps/frontend/components/Burger/Burger.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Link from 'next/link';
+
 import { SvgIcon } from '../SvgIcon/SvgIcon';
 import { useAuthStore } from '@/store/authStore';
 import Button from '../UI/Button/Button';
